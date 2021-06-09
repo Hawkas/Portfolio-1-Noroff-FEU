1a0377bbd9d7e24f3193623cf5eaab29e3e9b214
Torbjørn Haukås <69842749+Hawkas@users.noreply.github.com>
2021-06-10 01:12:44 +0200

    ok now everything is animated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const menuBar = document.querySelector(".navblock__bars");
 const lowerBlock = document.querySelector(".navblock__lower");
-const header = document.querySelector("body > header");
+const navHeader = document.querySelector(".page-container > header");
 const anchorArray = document.querySelectorAll("a");
 const sectionButtons = document.querySelectorAll(".sectiongrid__title button");
-
+const navLinks = document.querySelectorAll(".navblock__item, .header__button");
+const sectionContent = document.querySelectorAll(".content__wrap");
+let isRunning = false;
 /* To prevent jankiness from link click events */
 function noBubble(e) {
   e.stopPropagation();
@@ -22,8 +24,68 @@ function openMenu() {
     lowerBlock.classList.remove("navblock__lower--hide");
     lowerBlock.classList.add("navblock__lower--show");
   }
-  header.classList.toggle("sticky");
+  navHeader.classList.toggle("sticky");
 }
+
+// Collapsing animation for element of auto-adjusted height
+// This is the only way I found to do this without using absolute values for height: https://css-tricks.com/using-css-transitions-auto-dimensions/
+// I don't know how to solve this any other way so the code used here is more or less similar, rather than bloating it for no reason
+// My way of handling this with multiple sections is my own idea however, and I've made a few small changes to suit my needs and fix some bugs.
+function collapseElement(element, button) {
+  let trueHeight = element.scrollHeight;
+  element.style.transition = "none";
+  element.tabIndex = "-1";
+
+  requestAnimationFrame(function () {
+    element.style.height = trueHeight + "px";
+    element.style.transition = "height 0.5s ease-out";
+
+    requestAnimationFrame(function () {
+      element.style.height = 0 + "px";
+    });
+  });
+  setTimeout(function () {
+    element.style.display = "none";
+    element.style.transition = null;
+  }, 500);
+  button.classList.remove("expanded");
+  button.setAttribute("aria-expanded", "false");
+}
+
+function expandElement(element, button) {
+  element.style.display = "block";
+  let trueHeight = element.scrollHeight;
+  const scrollY = button.getBoundingClientRect().top + window.pageYOffset;
+  element.tabIndex = "0";
+  element.style.height = trueHeight + "px";
+
+  element.addEventListener("transitionend", function (e) {
+    // so it only triggers once
+    element.removeEventListener("transitionend", arguments.callee);
+    element.style.height = null;
+    window.scrollTo({ top: scrollY, behavior: "smooth" });
+  });
+  button.classList.add("expanded");
+  button.setAttribute("aria-expanded", "true");
+}
+
+// Remove no-js class. Just so site is usable even without JS.
+document.body.classList.remove("no-js");
+for (section of sectionContent) {
+  section.style.height = "0px";
+}
+
+// It seems Webkit browsers renders smallcaps slightly smaller than Gecko, which renders it as it should be. This is to remedy that.
+if (navigator.userAgent.includes("Chrome") || navigator.userAgent.includes("Safari")) {
+  const smallcaps = document.querySelectorAll(".smallcaps, .smallcaps--all");
+  for (let element of smallcaps) {
+    let text = element.innerHTML;
+    let weight = Number(window.getComputedStyle(element).getPropertyValue("font-weight"));
+    if (weight === 700) element.style.fontWeight = `${weight + 100}`;
+    element.innerHTML = `<span class="webkit-fix">${text}</span>`;
+  }
+}
+
 // Hide menu when pressing on the opaque bg
 lowerBlock.addEventListener("click", function (e) {
   if (document.querySelector(".navblock__lower--show") && e.offsetY > lowerBlock.offsetHeight) {
@@ -32,30 +94,43 @@ lowerBlock.addEventListener("click", function (e) {
 });
 
 menuBar.addEventListener("click", openMenu);
-for (let anchor of anchorArray) {
-  anchor.addEventListener("click", noBubble);
-}
+// for (let anchor of anchorArray) {
+//   anchor.addEventListener("click", noBubble);
+// }
 
 for (let button of sectionButtons) {
   button.addEventListener("click", function () {
+    // Don't run if already running
+    if (isRunning) return;
+    isRunning = true;
+    let section = this.id.replace("-header", "");
+    const sectionElement = document.querySelector(`#${section}-content`);
     if (this.classList.contains("expanded")) {
-      this.setAttribute("aria-expanded", "false");
+      collapseElement(sectionElement, this);
     } else {
-      this.setAttribute("aria-expanded", "true");
+      expandElement(sectionElement, this);
     }
-    this.classList.toggle("expanded");
+    setTimeout(function () {
+      isRunning = false;
+    }, 600);
+  });
+  button.addEventListener("mousedown", function (e) {
+    e.preventDefault();
   });
 }
-
-// It seems webkit renders smallcaps slightly smaller than Gecko and other browsers, this is to remedy that.
-if (navigator.userAgent.includes("Chrome") || navigator.userAgent.includes("Safari")) {
-  const smallcaps = document.querySelectorAll(".smallcaps, .smallcaps--all");
-  console.log(window.getComputedStyle(smallcaps[0]).getPropertyValue("font-size"));
-  for (let element of smallcaps) {
-    let text = element.innerHTML;
-    let weight = Number(window.getComputedStyle(element).getPropertyValue("font-weight"));
-    console.log(weight);
-    if (weight === 700) element.style.fontWeight = `${weight + 100}`;
-    element.innerHTML = `<span class="webkit-fix">${text}</span>`;
-  }
+for (let navLink of navLinks) {
+  navLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (this !== document.querySelector(".header__button") && document.querySelector(".navblock__lower--show")) {
+      openMenu();
+    }
+    let target = this.getAttribute("href");
+    let button = document.querySelector(`${target}`);
+    const scrollY = button.getBoundingClientRect().top + window.pageYOffset;
+    if (button.classList.contains("expanded")) window.scrollTo({ top: scrollY, behavior: "smooth" });
+    else {
+      let click = new Event("click");
+      button.dispatchEvent(click);
+    }
+  });
 }
