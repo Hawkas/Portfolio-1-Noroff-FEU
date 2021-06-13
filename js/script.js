82ae15566cba2374246eb64d168533521501f1a5
Torbjørn Haukås <69842749+Hawkas@users.noreply.github.com>
2021-06-13 21:13:56 +0200

    testing preload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+//! Dropdown Menu vars
 const menuBar = document.querySelector(".navblock__bars");
 const lowerBlock = document.querySelector(".navblock__lower");
 const navHeader = document.querySelector(".navblock");
 const navLinks = document.querySelectorAll(".navblock__item, .header__button");
 
+//! Section vars
 const sectionButtons = document.querySelectorAll(".sectiongrid__title button");
 let isRunning = false;
 
+//* Projects vars
 const projectButtons = document.querySelectorAll(".projects__button");
 const imageButtons = document.querySelectorAll(".projects__imagewrap");
 const modal = document.querySelector(".modal");
@@ -15,6 +18,15 @@ const modalImage = document.querySelector(".modal__image");
 let modalOpen = false;
 let lastFocusedElement = "";
 
+//! Fetch vars
+const out = document.querySelector(".content--blog");
+// const cors = "https://noroffcors.herokuapp.com/";
+const url = "../blogposts/wp-json/wp/v2/posts?per_page=3&orderby=date&_embed";
+let apiFetched = false;
+
+//! Functions
+
+//* Mobile Menu
 function closeMenu() {
   lowerBlock.classList.remove("navblock__lower--show");
   lowerBlock.classList.add("navblock__lower--vanish");
@@ -48,12 +60,12 @@ function toggleMenu() {
   }, 400);
 }
 
+//* Section
 // Collapsing animation for element of auto-adjusted height
 // The method I found to do this without using absolute values for height: https://css-tricks.com/using-css-transitions-auto-dimensions/
 function collapseElement(element, button) {
   let trueHeight = element.scrollHeight;
   element.style.transition = "none";
-  element.tabIndex = "-1";
 
   requestAnimationFrame(function () {
     element.style.height = trueHeight + "px";
@@ -64,7 +76,6 @@ function collapseElement(element, button) {
     });
   });
   element.addEventListener("transitionend", function (e) {
-    // so it only triggers once
     element.removeEventListener("transitionend", arguments.callee);
     element.style.display = null;
     element.style.transition = null;
@@ -77,17 +88,59 @@ function expandElement(element, button) {
   element.style.display = "block";
   let trueHeight = element.scrollHeight;
   const scrollY = button.getBoundingClientRect().top + window.pageYOffset;
-  element.tabIndex = "0";
   element.style.height = trueHeight + "px";
 
   element.addEventListener("transitionend", function (e) {
-    // so it only triggers once
     element.removeEventListener("transitionend", arguments.callee);
-    element.style.height = null;
+    element.style.height = "";
     window.scrollTo({ top: scrollY, behavior: "smooth" });
   });
   button.classList.add("expanded");
   button.setAttribute("aria-expanded", "true");
+}
+
+function toggleSections() {
+  if (isRunning) return;
+  isRunning = true;
+  let section = this.id.replace("-title", "");
+  const sectionElement = document.querySelector(`#${section}-content`);
+  if (this.classList.contains("expanded")) {
+    collapseElement(sectionElement, this);
+  } else {
+    if (this.id === "blog-title" && !apiFetched) fetchBlogs(url);
+    expandElement(sectionElement, this);
+  }
+  setTimeout(function () {
+    isRunning = false;
+  }, 600);
+}
+
+function toggleProjects() {
+  if (isRunning) return;
+  isRunning = true;
+  let project = document.querySelector(`#${this.id}-details`);
+  if (this.classList.contains("expanded")) {
+    this.innerHTML = this.innerHTML.replace("Read less&nbsp;", "Read more");
+    collapseElement(project, this);
+  } else {
+    this.innerHTML = this.innerHTML.replace("Read more", "Read less&nbsp;");
+    this.classList.toggle("flipped");
+    expandElement(project, this);
+  }
+  setTimeout(function () {
+    isRunning = false;
+  }, 600);
+}
+function openModal() {
+  let image = this.firstElementChild;
+  lastFocusedElement = this;
+  modalOpen = true;
+  modal.style.display = "block";
+  modalImage.width = image.dataset.width;
+  modalImage.height = image.dataset.height;
+  modalImage.src = image.dataset.modal;
+  modalCaption.innerHTML = image.alt;
+  modal.focus();
 }
 
 function closeModal(e) {
@@ -107,19 +160,107 @@ function blurOnClick(button) {
     e.preventDefault();
   });
 }
-// Remove no-js class. Just so site is usable even without JS.
-document.body.classList.remove("no-js");
 
-// It seems Webkit browsers renders smallcaps slightly smaller than Gecko, which renders it as it should be. This is to remedy that.
-if (navigator.userAgent.includes("Chrome") || navigator.userAgent.includes("Safari")) {
-  const smallcaps = document.querySelectorAll(".smallcaps, .smallcaps--all");
-  for (let element of smallcaps) {
-    let text = element.innerHTML;
-    let weight = Number(window.getComputedStyle(element).getPropertyValue("font-weight"));
-    if (weight === 700) element.style.fontWeight = `${weight + 100}`;
-    element.innerHTML = `<span class="webkit-fix">${text}</span>`;
+//* API Functions
+function checkCategories(categoriesList) {
+  if (categoriesList.length > 1) {
+    for (let categories of categoriesList) {
+      if (categories.name === "Featured") continue;
+      return categories.id;
+    }
+  } else return categoriesList.id;
+}
+function dateOrdinal(date) {
+  date = parseInt(date);
+  switch (date) {
+    case 1:
+    case 21:
+    case 31:
+      return date + "st";
+    case 2:
+    case 22:
+      return date + "nd";
+    case 3:
+    case 23:
+      return date + "rd";
+    default:
+      return date + "th";
   }
 }
+function dateHandler(rawDate) {
+  let postDate = new Date(rawDate);
+  let options = { month: "long" };
+  const dateObject = {
+    day: dateOrdinal(postDate.getDate()) || "Unknown",
+    month: new Intl.DateTimeFormat("en-US", options).format(postDate) || "Unknown",
+    year: postDate.getFullYear() || "Unknown",
+    time: rawDate || "Unknown",
+  };
+  return dateObject;
+}
+
+//! Fetch API
+function wrapNumber(match) {
+  let newString = `${match[0]}<span class="ampersand-fix">${match[1]}</span>`;
+  return newString;
+}
+
+function postBlog(postList) {
+  let newHtml = "";
+  for (let post of postList) {
+    let category = checkCategories(post._embedded["wp:term"][0]);
+    let date = dateHandler(post.date);
+    let link = "https://dreamy-shaw-314388.netlify.app/post.html/post.html?id=" + post.id + "&category=" + category;
+    let img = {
+      path: post._embedded["wp:featuredmedia"][0],
+      get url() {
+        return this.path["media_details"].sizes.medium_large.source_url;
+      },
+      get alt() {
+        return this.path.alt_text;
+      },
+    };
+    // I really regret small caps now. I have to adjust numbers and symbols of unknown values.
+    let title = post.title.rendered;
+    title = title.replace(/[a-z][0-9]/i, wrapNumber);
+    title = title.replace("&#038;", `<span class="ampersand-fix">&#038;</span>`);
+    newHtml += `
+    <li>
+      <article class="blog">
+        <a href="${link}" class="blog__linkwrap" rel="noreferrer noopener" target="_blank">
+          <div class="blog__imagewrap">
+            <img loading="lazy" src="${img.url}" width="768" height="320" alt="${img.alt}" />
+          </div>
+          <div class="blog__textblock">
+            <h3 class="blog__header smallcaps--all">${title}</h3>
+            <p>${post.excerpt.rendered}</p>
+            <time datetime="${date.time}">${date.month} ${date.day}, ${date.year}</time>
+          </div>
+        </a>
+      </article>
+    </li>`;
+  }
+  return newHtml;
+}
+async function fetchBlogs(url = "") {
+  fetch(url, {
+    method: "GET",
+    mode: "same-origin",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      out.innerHTML = postBlog(data);
+      apiFetched = true;
+    })
+    .catch((error) => {
+      out.innerHTML = `<h3 style="color: red">Something went wrong, refresh and try again</h3>`;
+      console.error(error);
+    });
+}
+
+//! Expressions & Event Listeners
+// Remove no-js class. Just so site is usable even without JS.
+document.body.classList.remove("no-js");
 
 // Hide menu when pressing on the opaque bg
 lowerBlock.addEventListener("click", function (e) {
@@ -135,20 +276,7 @@ for (let button of sectionButtons) {
   let sectionWrap = document.querySelector(`#${section}-content`);
   sectionWrap.style.height = "0px";
   blurOnClick(button);
-  button.addEventListener("click", function () {
-    // Don't run if already running
-    if (isRunning) return;
-    isRunning = true;
-    const sectionElement = sectionWrap;
-    if (this.classList.contains("expanded")) {
-      collapseElement(sectionElement, this);
-    } else {
-      expandElement(sectionElement, this);
-    }
-    setTimeout(function () {
-      isRunning = false;
-    }, 600);
-  });
+  button.addEventListener("click", toggleSections);
 }
 for (let navLink of navLinks) {
   navLink.addEventListener("click", function (e) {
@@ -171,22 +299,7 @@ for (let button of projectButtons) {
   let projectWrap = document.querySelector(`#${button.id}-details`);
   projectWrap.style.height = "0px";
   blurOnClick(button);
-  button.addEventListener("click", function () {
-    if (isRunning) return;
-    isRunning = true;
-    const project = projectWrap;
-    if (this.classList.contains("expanded")) {
-      this.innerHTML = this.innerHTML.replace("Read less&nbsp;", "Read more");
-      collapseElement(project, this);
-    } else {
-      this.innerHTML = this.innerHTML.replace("Read more", "Read less&nbsp;");
-      this.classList.toggle("flipped");
-      expandElement(project, this);
-    }
-    setTimeout(function () {
-      isRunning = false;
-    }, 600);
-  });
+  button.addEventListener("click", toggleProjects);
 }
 
 modal.addEventListener("click", closeModal);
@@ -197,17 +310,7 @@ modalImage.addEventListener("click", function (e) {
 });
 
 for (let button of imageButtons) {
-  let image = button.firstElementChild;
-  button.addEventListener("click", function () {
-    lastFocusedElement = button;
-    modalOpen = true;
-    modal.style.display = "block";
-    modalImage.width = image.dataset.width;
-    modalImage.height = image.dataset.height;
-    modalImage.src = image.dataset.modal;
-    modalCaption.innerHTML = image.alt;
-    modal.focus();
-  });
+  button.addEventListener("click", openModal);
 }
 
 // To trap focus in modal and return to last location after
